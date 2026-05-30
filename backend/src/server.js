a5391e8bb5b19ad12433982108910050d90d1f98
Sarwat Mustafa <sarwatmustafa03@gmail.com>
2026-05-30 13:03:33 +0500

    login works

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,45 @@
 require("dotenv").config();
-const cors = require("cors");
 
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+// database
 const connectDB = require("./config/db.config");
-connectDB();
 
-app.use(cors( {
-    origin: "http://localhost:3000", // frontend URL (React)
+// middlewares
+app.use(cors({
+    origin: "http://localhost:5175",
     credentials: true
- }));
+}));
+
+
 app.use(express.json());
 
+// routes
 const authRoutes = require("./routes/auth.routes");
+app.use("/api/auth", authRoutes);
 
-app.use("/api/auth", authRoutes);//req ko route tak bhejty
+app.get("/", (req, res) => {
+    res.send("API running...");
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`);
-});
+
+//FIX: start server only AFTER DB connects
+const startServer = async () => {
+    try {
+        await connectDB();  
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on ${PORT}`);
+        });
+
+    } catch (error) {
+        console.error("Server startup failed:", error);
+        process.exit(1);
+    }
+};
+
+startServer();
